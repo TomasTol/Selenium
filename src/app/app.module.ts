cd5ec57afc648c0733308efee90b4a8862c5233c
TomasTol <110826341+TomasTol@users.noreply.github.com>
2023-08-28 14:17:17 -0300

    commiteado

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Route, Routes } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+const appRoutes:Routes=[
+  {path: 'login', component:LoginComponent},
+]
